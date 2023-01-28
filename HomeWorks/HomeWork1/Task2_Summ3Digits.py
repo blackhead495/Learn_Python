6923a3ecf1dddfbe4e96593331b938938f4a3b87
Profi <faris-d@mail.ru>
2023-01-28 16:33:16 +0300

    Решил задачу 2

--- HomeWorks/HomeWork1/Task2_Summ3Digits.py
+++ HomeWorks/HomeWork1/Task2_Summ3Digits.py
@@ -2,23 +2,41 @@
 #       123 -> 6 (1 + 2 + 3)
 #       100 -> 1 (1 + 0 + 0)
 
-num = int(input('Введите трёхзначное число : '))
-tmp = num
+# ------ Способ 1 ----------------------------------------
 
-lst = [0, 0, 0]
+# tmp = num = int(input('Введите трёхзначное число : '))
+#
+# d1 = tmp - tmp // 10 * 10
+# tmp //= 10
+# d2 = tmp - tmp // 10 * 10
+# tmp //= 10
+# d3 = tmp - tmp // 10 * 10
+# tmp //= 10
+#
+# # print(num, '->', d1 + d2 + d3, '(', d3, '+', d2, '+', d1, ')')
+# print(f'{num} -> {d1 + d2 + d3} ({d3} + {d2} + {d1})')
 
-d1 = tmp - tmp // 10 * 10
-tmp //= 10
-d2 = tmp - tmp // 10 * 10
-tmp //= 10
-d3 = tmp - tmp // 10 * 10
-tmp //= 10
+# ------- Способ 2 ------------------------------------
 
-# for i in range(3):
+tmp = num = int(input('Введите трёхзначное число : '))
+summa = 0
+lst = []
 
+while tmp:
+    d = tmp - tmp // 10 * 10
+    lst.append(d)
+    summa += d
+    tmp //= 10
 
-print(num, '->', d1 + d2 + d3, '(', d3, '+', d2, '+', d1, ')')
-print(f'{num} -> {d1 + d2 + d3} ( {d3} + {d2} + {d1} )')
+lst.reverse()
+
+print(num, '->', summa, '(', *lst, ')')
+
+# print(f'{num} -> {summa} ( ', end='')
+# for i in range(len(lst)):
+#     print(lst[i], end=' ')
+# else:
+#     print(')')
 
 
 
